c70666024c8b7baa67f78f2bee2027e6bd3e3a13
Jim <jim@cubixmc.org>
2015-01-13 18:45:32 +0100

    Added isDamagable boolean

--- src/main/java/org/cubixmc/entity/LivingEntity.java
+++ src/main/java/org/cubixmc/entity/LivingEntity.java
@@ -1,6 +1,13 @@
 package org.cubixmc.entity;
 
 public interface LivingEntity extends Entity {
+ 
+     /**
+     * Get if the living entity is damagable.
+     *
+     * @return if the entity is damagable
+     */
+    public boolean isDamagable();
 
     /**
      * Get the current health of the entity.
@@ -16,11 +23,6 @@ public interface LivingEntity extends Entity {
      * @param health New health for the entity
      */
     public void setHealth(double health);
-    /**
-     * If the developer is adding damage to an entity manually, they would have to use damageEntity();
-     * damageEntity(); applies for all entities the void is being called to.
-     */
-    public void damageEntity(double damage);
 
     /**
      * Get the maximum amount of health the entity can have.
