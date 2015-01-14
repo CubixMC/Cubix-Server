92d73b7deac3c3161431f16fabea103adce6330f
Kazoo <gnkazoo@gmail.com>
2015-01-14 12:39:37 +0100

    Added a new method for calling events as cancelled

--- src/main/java/org/cubixmc/event/EventCaller.java
+++ src/main/java/org/cubixmc/event/EventCaller.java
@@ -1,9 +1,9 @@
 package org.cubixmc.event
 
+import java.lang.annotation.Target;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
 
 /**
  * @author Giovanni
@@ -13,8 +13,6 @@ import java.lang.annotation.Target;
 @Target(ElementType.METHOD)
    @Retention(RetentionPolicy.RUNTIME)
    public @interface Events {
-
-
 
     /**
      * @boolean type(default, false);
