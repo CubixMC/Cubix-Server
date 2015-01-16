46ce8c3e542a7175f4e539830ed4612a7c76e8fd
Lennart ten Wolde <0p1q9o2w@hotmail.nl>
2015-01-16 19:57:42 +0100

    Update vectors

--- src/main/java/org/cubixmc/util/Vector3I.java
+++ src/main/java/org/cubixmc/util/Vector3I.java
@@ -1,37 +1,86 @@
 package org.cubixmc.util;
 
-
-/**
-*
-* @author Jim Bergens
-*/
-
-
 public class Vector3I {
+    private int x;
+    private int y;
+    private int z;
 
-//TODO: Vector2D 
-
-
-private Vector3I v3i;
-// private Vector2D v2d;
-private Vector3D v3d;
-
-
-   @Override
-    public String toString() {
-        return "Vector3I{" + v3i + "}";
+    public Vector3I(double x, double y, double z) {
+        this(MathHelper.floor(x), MathHelper.floor(y), MathHelper.floor(z));
     }
-    
+
+    public Vector3I(int x, int y, int z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getZ() {
+        return z;
+    }
+
+    public void setZ(int z) {
+        this.z = z;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    /**
+     * Return a Vector3D for the current vector.
+     *
+     * @return A new instance of {@link org.cubixmc.util.Vector3D Vector3D}
+     */
+    public Vector3D toVector3D() {
+        return new Vector3D(x, y, z);
+    }
+
     @Override
-    public String toVector3D() {
-    return "Vector3D{" + v3d + "}";
+    public Vector3I clone() {
+        return new Vector3I(x, y, z);
     }
-    
-    
-    //========================[WIP]======================
-    // @Override
-    // public String toVector2D() {
-    // return "Vector2D{" + v2d + "}";
-    // }
 
+    @Override
+    public boolean equals(Object o) {
+        if(this == o) return true;
+        if(o == null || getClass() != o.getClass()) return false;
+
+        Vector3I vector3I = (Vector3I) o;
+
+        if(x != vector3I.x) return false;
+        if(y != vector3I.y) return false;
+        if(z != vector3I.z) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = x;
+        result = 31 * result + y;
+        result = 31 * result + z;
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Vector3I{" +
+                "x=" + x +
+                ", y=" + y +
+                ", z=" + z +
+                '}';
+    }
 }
