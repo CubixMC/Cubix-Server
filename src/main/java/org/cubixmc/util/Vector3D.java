9995bab1dde870a661cd17d12ddb2ec61ea423e8
Jim <jim@cubixmc.org>
2015-01-16 12:24:05 +0100

    Correct if incorrect - Inital Commit

---
+++ src/main/java/org/cubixmc/util/Vector3D.java
@@ -0,0 +1,13 @@
+package org.cubixmc.util
+
+
+import org.cubixmc.util.Position
+
+public class Vector3D {
+private Position pos;
+
+ public void multiply(double TimesThePosition){
+        return pos.getX() * TimesThePosition, pos.getY() * TimesThePosition, pos.getZ() * TimesThePosition;
+    }
+
+}
