afed5b5955b1b568037de80370657cea85d14224
Phobia <docstuts@gmail.com>
2015-01-16 22:34:10 +1100

    Added Vector3D

--- src/main/java/org/cubixmc/util/Vector3D.java
+++ src/main/java/org/cubixmc/util/Vector3D.java
@@ -1,13 +1,101 @@
 package org.cubixmc.util;
 
-
-import org.cubixmc.util.Position;
-
 public class Vector3D {
-private Position pos;
 
- public Position multiply(double TimesThePosition){
-        return pos.getX() * TimesThePosition, pos.getY() * TimesThePosition, pos.getZ() * TimesThePosition);
+    private double x,y,z;
+
+
+    /**
+     * Construct the vector with all components as 0.
+     */
+    public Vector3D() {
+        this(0,0,0);
     }
+    /**
+     * Construct the vector with provided integer components.
+     *
+     * @param x X component
+     * @param y Y component
+     * @param z Z component
+     */
+    public Vector3D(int x, int y, int z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+    /**
+     * Construct the vector with provided double components.
+     *
+     * @param x X component
+     * @param y Y component
+     * @param z Z component
+     */
+    public Vector3D(double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+    /**
+     * Construct the vector with provided float components.
+     *
+     * @param x X component
+     * @param y Y component
+     * @param z Z component
+     */
+    public Vector3D(float x, float y, float z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+    /**
+     * Adds a vector to this one
+     *
+     * @param vec The other vector
+     * @return the same vector
+     */
+    public Vector3D add(Vector3D vec) {
+        x += vec.x;
+        y += vec.y;
+        z += vec.z;
+        return this;
+    }
+    /**
+     * Subtracts a vector from this one.
+     *
+     * @param vec The other vector
+     * @return the same vector
+     */
+    public Vector3D subtract(Vector3D vec) {
+        x -= vec.x;
+        y -= vec.y;
+        z -= vec.z;
+        return this;
+    }
+    /**
+     * Multiplies the vector by another.
+     *
+     * @param vec The other vector
+     * @return the same vector
+     */
+    public Vector3D multiply(Vector3D vec) {
+        x *= vec.x;
+        y *= vec.y;
+        z *= vec.z;
+        return this;
+    }
+
+    /**
+     * Divides the vector by another.
+     *
+     * @param vec The other vector
+     * @return the same vector
+     */
+    public Vector3D divide(Vector3D vec) {
+        x /= vec.x;
+        y /= vec.y;
+        z /= vec.z;
+        return this;
+    }
+
 
 }
