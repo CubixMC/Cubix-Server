58396546a2fb1939a3b7bcb82bacfaa4c5c4d118
Jim <jim@cubixmc.org>
2015-01-16 12:26:27 +0100

    Changed Something.

--- src/main/java/org/cubixmc/util/Vector3D.java
+++ src/main/java/org/cubixmc/util/Vector3D.java
@@ -6,7 +6,7 @@ import org.cubixmc.util.Position;
 public class Vector3D {
 private Position pos;
 
- public void multiply(double TimesThePosition){
+ public Position multiply(double TimesThePosition){
         return pos.getX() * TimesThePosition, pos.getY() * TimesThePosition, pos.getZ() * TimesThePosition);
     }
 
